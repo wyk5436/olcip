8a4b79f14e5232cce109f3aa9995776bfa011679
wyk <wyk5436@outlook.com>
2024-03-24 18:28:10 -0400

    Working learning (not updating while controlling yet) S-function for the crane problem

--- simulation/online_learning_S.m
+++ simulation/online_learning_S.m
@@ -5,7 +5,7 @@ setup(block);
 function setup(block)
 
   % Register the number of ports.
-  block.NumInputPorts  = 1;
+  block.NumInputPorts  = 2;
   block.NumOutputPorts = 1;
   
   % Set up the port properties to be inherited or dynamic.
@@ -15,199 +15,35 @@ function setup(block)
   block.InputPort(1).Complexity   = 'Real'; 
   block.InputPort(1).DataTypeId   = 0;
   block.InputPort(1).SamplingMode = 'Sample';
-  block.InputPort(1).Dimensions   = 4;   
+  block.InputPort(1).Dimensions   = 4;  
+  
+  block.InputPort(2).Complexity   = 'Real'; 
+  block.InputPort(2).DataTypeId   = 0;
+  block.InputPort(2).SamplingMode = 'Sample';
+  block.InputPort(2).Dimensions   = 1;  
 
   block.OutputPort(1).Complexity   = 'Real';
   block.OutputPort(1).DataTypeId   = 0;
   block.OutputPort(1).SamplingMode = 'Sample';
-  block.OutputPort(1).Dimensions   = 4;
+  block.OutputPort(1).Dimensions   = 1;
 
-%   % Register the parameters.
-%   block.NumDialogPrms     = 3;
-%   block.DialogPrmsTunable = {'Tunable','Nontunable','SimOnlyTunable'};
-  
-%   % Set up the continuous states.
-%   block.NumContStates = 1;
-
-  % Register the sample times.
-  %  [0 offset]            : Continuous sample time
-  %  [positive_num offset] : Discrete sample time
-  %
-  %  [-1, 0]               : Inherited sample time
-  %  [-2, 0]               : Variable sample time
   block.SampleTimes = [-1 0];
-  
-  % -----------------------------------------------------------------
-  % Options
-  % -----------------------------------------------------------------
-  % Specify if Accelerator should use TLC or call back to the 
-  % MATLAB file
-  block.SetAccelRunOnTLC(true);
-  
-  % Specify the block's operating point compliance. The block operating 
-  % point is used during the containing model's operating point save/restore)
-  % The allowed values are:
-  %   'Default' : Same the block's operating point as of a built-in block
-  %   'UseEmpty': No data to save/restore in the block operating point
-  %   'Custom'  : Has custom methods for operating point save/restore
-  %                 (see GetOperatingPoint/SetOperatingPoint below)
-  %   'Disallow': Error out when saving or restoring the block operating point.
   block.OperatingPointCompliance = 'Default';
   
-  % -----------------------------------------------------------------
-  % The MATLAB S-function uses an internal registry for all
-  % block methods. You should register all relevant methods
-  % (optional and required) as illustrated below. You may choose
-  % any suitable name for the methods and implement these methods
-  % as local functions within the same file.
-  % -----------------------------------------------------------------
-   
-  % -----------------------------------------------------------------
-  % Register the methods called during update diagram/compilation.
-  % -----------------------------------------------------------------
-  
-
-  %
-  % SetInputPortSamplingMode:
-  %   Functionality    : Check and set input and output port 
-  %                      attributes and specify whether the port is operating 
-  %                      in sample-based or frame-based mode
-  %   C MEX counterpart: mdlSetInputPortFrameData.
-  %   (The DSP System Toolbox is required to set a port as frame-based)
-  %
   block.RegBlockMethod('SetInputPortSamplingMode', @SetInpPortFrameData);
-  
-  %
-  % SetInputPortDimensions:
-  %   Functionality    : Check and set the input and optionally the output
-  %                      port dimensions.
-  %   C MEX counterpart: mdlSetInputPortDimensionInfo
-  %
   block.RegBlockMethod('SetInputPortDimensions', @SetInpPortDims);
-
-  %
-  % SetOutputPortDimensions:
-  %   Functionality    : Check and set the output and optionally the input
-  %                      port dimensions.
-  %   C MEX counterpart: mdlSetOutputPortDimensionInfo
-  %
   block.RegBlockMethod('SetOutputPortDimensions', @SetOutPortDims);
-  
-  %
-  % SetInputPortDatatype:
-  %   Functionality    : Check and set the input and optionally the output
-  %                      port datatypes.
-  %   C MEX counterpart: mdlSetInputPortDataType
-  %
   block.RegBlockMethod('SetInputPortDataType', @SetInpPortDataType);
-  
-  %
-  % SetOutputPortDatatype:
-  %   Functionality    : Check and set the output and optionally the input
-  %                      port datatypes.
-  %   C MEX counterpart: mdlSetOutputPortDataType
-  %
   block.RegBlockMethod('SetOutputPortDataType', @SetOutPortDataType);
-  
-  %
-  % PostPropagationSetup:
-  %   Functionality    : Set up the work areas and the state variables. You can
-  %                      also register run-time methods here.
-  %   C MEX counterpart: mdlSetWorkWidths
-  %
   block.RegBlockMethod('PostPropagationSetup', @DoPostPropSetup);
 
   % -----------------------------------------------------------------
   % Register methods called at run-time
   % -----------------------------------------------------------------
   
-  
-  % 
-  % Start:
-  %   Functionality    : Call to initialize the state and the work
-  %                      area values.
-  %   C MEX counterpart: mdlStart
-  %
-  block.RegBlockMethod('Start', @Start);
-
-  % 
-  % Outputs:
-  %   Functionality    : Call to generate the block outputs during a
-  %                      simulation step.
-  %   C MEX counterpart: mdlOutputs
-  %
   block.RegBlockMethod('Outputs', @Outputs);
-
-  % 
-  % Update:
-  %   Functionality    : Call to update the discrete states
-  %                      during a simulation step.
-  %   C MEX counterpart: mdlUpdate
-  %
   block.RegBlockMethod('Update', @Update);
-
-  % 
-  % Derivatives:
-  %   Functionality    : Call to update the derivatives of the
-  %                      continuous states during a simulation step.
-  %   C MEX counterpart: mdlDerivatives
-  %
-  block.RegBlockMethod('Derivatives', @Derivatives);
   
-  % 
-  % Projection:
-  %   Functionality    : Call to update the projections during a
-  %                      simulation step.
-  %   C MEX counterpart: mdlProjections
-  %
-  block.RegBlockMethod('Projection', @Projection);
-  
-  % 
-  % SimStatusChange:
-  %   Functionality    : Call when simulation enters pause mode
-  %                      or leaves pause mode.
-  %   C MEX counterpart: mdlSimStatusChange
-  %
-  block.RegBlockMethod('SimStatusChange', @SimStatusChange);
-  
-  % 
-  % Terminate:
-  %   Functionality    : Call at the end of a simulation for cleanup.
-  %   C MEX counterpart: mdlTerminate
-  %
-  block.RegBlockMethod('Terminate', @Terminate);
-
-  %
-  % GetOperatingPoint:
-  %   Functionality    : Return the operating point of the block.
-  %   C MEX counterpart: mdlGetOperatingPoint
-  %
-  block.RegBlockMethod('GetOperatingPoint', @GetOperatingPoint);
-  
-  %
-  % SetOperatingPoint:
-  %   Functionality    : Set the operating point data of the block using
-  %                       from the given value.
-  %   C MEX counterpart: mdlSetOperatingPoint
-  %
-  block.RegBlockMethod('SetOperatingPoint', @SetOperatingPoint);
-
-  % -----------------------------------------------------------------
-  % Register the methods called during code generation.
-  % -----------------------------------------------------------------
-  
-%   %
-%   % WriteRTW:
-%   %   Functionality    : Write specific information to model.rtw file.
-%   %   C MEX counterpart: mdlRTW
-%   %
-%   block.RegBlockMethod('WriteRTW', @WriteRTW);
-% %endfunction
-
-% -------------------------------------------------------------------
-% The local functions below are provided to illustrate how you may implement
-% the various block methods listed above.
-% -------------------------------------------------------------------
 
 
 function SetInpPortFrameData(block, idx, fd)
@@ -247,19 +83,25 @@ function SetOutPortDataType(block, idx, dt)
 
     
 function DoPostPropSetup(block)
-  block.NumDworks = 1;
+  block.NumDworks = 3;
   
   block.Dwork(1).Name            = 'state';
-  block.Dwork(1).Dimensions      = 4;
+  block.Dwork(1).Dimensions      = 4*2000;
   block.Dwork(1).DatatypeID      = 0;      % double
   block.Dwork(1).Complexity      = 'Real'; % real
   block.Dwork(1).UsedAsDiscState = true;
   
-%   block.Dwork(2).Name            = 'numPause';
-%   block.Dwork(2).Dimensions      = 1;
-%   block.Dwork(2).DatatypeID      = 7;      % uint32
-%   block.Dwork(2).Complexity      = 'Real'; % real
-%   block.Dwork(2).UsedAsDiscState = true;
+  block.Dwork(2).Name            = 'time_called';
+  block.Dwork(2).Dimensions      = 1;
+  block.Dwork(2).DatatypeID      = 0;      % double
+  block.Dwork(2).Complexity      = 'Real'; % real
+  block.Dwork(2).UsedAsDiscState = true;
+  
+  block.Dwork(3).Name            = 'control_gain';
+  block.Dwork(3).Dimensions      = 4;
+  block.Dwork(3).DatatypeID      = 0;      % double
+  block.Dwork(3).Complexity      = 'Real'; % real
+  block.Dwork(3).UsedAsDiscState = true;
   
   % Register all tunable parameters as runtime parameters.
   block.AutoRegRuntimePrms;
@@ -267,71 +109,59 @@ function DoPostPropSetup(block)
 %endfunction
 
 
-function Start(block)
-
-  block.Dwork(1).Data = 0;
-   
-%endfunction
-
-% function WriteRTW(block)
-%   
-%    block.WriteRTWParam('matrix', 'M',    [1 2; 3 4]);
-%    block.WriteRTWParam('string', 'Mode', 'Auto');
-   
-%endfunction
-
 function Outputs(block)
+  K1 = 0.2065;
+  J = 0.0076;
+  l = 0.337;
+  r = 0.216;
+  B = [0; 0; K1/J;-r*K1/(J*l)];
+  Q = diag([5 30 0 0]);
+  R = 1;
+  dt = 0.01;
   
-  block.OutputPort(1).Data = block.Dwork(1).Data + block.InputPort(1).Data;
+  i = block.Dwork(2).Data;
+  state = block.Dwork(1).Data;
+  x = block.InputPort(1).Data;
+  if (block.InputPort(2).Data == 3)
+      idx = (i-1)*4 + 1;
+      state_data = state(1:(idx + 3));
+      mat = reshape(state_data,[4,i]);
+      X = mat(:,1:i-1);
+      Y = mat(:,2:end);
+      Am = Y*pinv(X);
+      
+      sys = ss(zeros(4),B,eye(4),0);
+      dis_sys = c2d(sys,dt);
+      disc_B = dis_sys.B;
+      K = dlqr(Am,disc_B,Q,R);
+      block.Dwork(3).Data = K;
+      block.OutputPort(1).Data = -K*x;
+  elseif (block.InputPort(2).Data > 3)
+      gain = block.Dwork(3).Data;
+      gain = reshape(gain,[1,4]);
+      block.OutputPort(1).Data = -gain*x;
+  end
   
 %endfunction
 
 function Update(block)
-  
-  block.Dwork(1).Data = block.InputPort(1).Data;
-  
-%endfunction
+  if (block.InputPort(2).Data > 2) && (block.InputPort(2).Data < 3)
+      block.Dwork(2).Data = block.Dwork(2).Data + 1;
+      i = block.Dwork(2).Data;
+      state = block.Dwork(1).Data;
+      x = block.InputPort(1).Data;
 
-function Derivatives(block)
-
-block.Derivatives.Data = 2*block.ContStates.Data;
-
-%endfunction
-
-function Projection(block)
-
-states = block.ContStates.Data;
-block.ContStates.Data = states+eps; 
-
-%endfunction
-
-function SimStatusChange(block, s)
-  
-  block.Dwork(2).Data = block.Dwork(2).Data+1;    
-
-  if s == 0
-    disp('Pause in simulation.');
-  elseif s == 1
-    disp('Resume simulation.');
+      if i <= 2000
+          idx = (i-1)*4 + 1;
+          state(idx:(idx+3)) = x;
+          block.Dwork(1).Data = state;
+          state_data = state(1:idx + 3);
+      else
+          state(1:1997) = state(4:2000);
+          state(1997:2000) = x;
+          block.Dwork(1).Data = state;
+      end
   end
   
 %endfunction
-    
-function Terminate(block)
 
-disp(['Terminating the block with handle ' num2str(block.BlockHandle) '.']);
-
-%endfunction
- 
-function operPointData = GetOperatingPoint(block)
-% package the Dwork data as the entire operating point of this block
-operPointData = block.Dwork(1).Data;
-
-%endfunction
-
-function SetOperatingPoint(block, operPointData)
-% the operating point of this block is the Dwork data (this method 
-% typically performs the inverse actions of the method GetOperatingPoint)
-block.Dwork(1).Data = operPointData;
-
-%endfunction
