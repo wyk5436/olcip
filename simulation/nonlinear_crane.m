227f3cce503fab7fdff1d162755bb7039ad994e5
wyk <wyk5436@outlook.com>
2024-03-24 18:31:21 -0400

    Corrected nonlinear crane equations

--- simulation/nonlinear_crane.m
+++ simulation/nonlinear_crane.m
@@ -18,11 +18,11 @@ function f = nonlinear_crane(t,x,u)
     tau = K1*u - K2*alpha_dot;
 
     denom = J + m*r^2 + m*l^2*sin(theta)^2-m*r^2*cos(theta)^2;
-    alpha_ddot = tau-(m*r*l*cos(theta)*(alphad^2*sin(theta)*cos(theta)-g*sin(theta)/l)+m*r*l*thetad^2*sin(theta) - 2*m*l^2*alphad*thetad*sin(theta)*cos(theta)) / denom;
+    alpha_ddot = (tau-m*r*l*cos(theta)*(alphad^2*sin(theta)*cos(theta)-g*sin(theta)/l)+m*r*l*thetad^2*sin(theta) - 2*m*l^2*alphad*thetad*sin(theta)*cos(theta)) / denom;
 
     K = J + m*r^2+m*l^2*sin(theta)^2;
     thetaddot_numer = -(r/l)*(tau + m*r*l*thetad^2*sin(theta)-2*m*l^2*alphad*thetad*sin(theta)*cos(theta))*cos(theta)/K + alphad^2*sin(theta)*cos(theta)-g*sin(theta)/l;
-    thetaddot_denom = 1 + m*r^2*cos(theta)^2/K;
+    thetaddot_denom = 1 - m*r^2*cos(theta)^2/K;
     theta_ddot = thetaddot_numer/thetaddot_denom;
     f = [alpha_dot;theta_dot;alpha_ddot;theta_ddot];
 end
