45daa4a3e14af060ca47c0e7cb5b954bed7b2216
Yangkai Wei <56571804+wyk5436@users.noreply.github.com>
2024-07-05 18:14:25 -0400

    Delete simulation/debug directory

--- simulation/debug/rk4c.m
+++
@@ -1,7 +0,0 @@
-function yy = rk4c(t,h,y,K,f)
-    K1 = f(t,y,-K*y);
-    K2 = f(t + h/2,y + h*K1/2,-K*(y + h*K1/2));
-    K3 = f(t + h/2,y + h*K2/2,-K*(y + h*K2/2));
-    K4 = f(t + h, y + h*K3, -K*(y + h*K3));
-    yy = y + (h/6)*(K1 + 2*K2 + 2*K3 + K4);
-end
